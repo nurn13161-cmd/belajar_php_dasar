877fcafec661d06af03858da906880129a63611a
nurn13161-cmd <nurn13161@gmail.com>
2026-08-06 08:14:45 +0700

    Tugas_PHP

---
+++ PHP_DASAR_PERTEMUAN_3_NOVI/if_else.php
@@ -0,0 +1,7 @@
+<?php
+$umur = 30;
+if ($umur > 18) {
+    echo "Dewasa";
+} else {
+    echo "Anak-anak";
+}
---
+++ PHP_DASAR_PERTEMUAN_3_NOVI/switch.php
@@ -0,0 +1,16 @@
+<?php
+$warna = "oren";
+
+switch ($warna) {
+    case "oren":
+        echo "Stop!";
+        break;
+    case "kuning":
+        echo "Hati-hati!";
+        break;
+    case "hijau":
+        echo "Jalan!";
+        break;
+    default:
+        echo "Warna tidak dikenali";
+}
